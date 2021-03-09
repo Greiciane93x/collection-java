008dcab3aa586a60a79f426132325ca90e731c78
greiciane <greiciane_araujo@hotmail.com>
2021-03-08 23:59:09 -0300

    hashmap

---
+++ listas-projeto/src/HashMapTesting.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+
+public class HashMapTesting {
+
+	public static void main(String[] args) {
+		HashMap<Integer, String> hmap = new HashMap<Integer, String>(); 
+		
+		hmap.put(1, "Aa"); 
+		hmap.put(2, "Bb"); 
+		hmap.put(3, "Cc"); 
+		hmap.put(4, "Dd"); 
+		hmap.put(5, "Ee"); 
+		hmap.put(6, "Ff"); 
+		hmap.put(7, "Gg"); 
+		hmap.put(8, "Hh"); 
+		hmap.put(9, "Ii"); 
+		hmap.put(10, "Jj"); 
+		
+		System.out.println("HashMap tem: " + hmap);
+		
+		
+	}
+}
